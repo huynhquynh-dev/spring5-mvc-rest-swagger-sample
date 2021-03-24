9a0ab6f3f3947a26fcadeb5a7062fcbb8b98e64b
huynhquynh-dev <huynhquynh-dev>
2021-03-24 09:32:54 +0700

    Added swagger UI

--- src/main/java/guru/springfamework/config/SwaggerConfig.java
+++ src/main/java/guru/springfamework/config/SwaggerConfig.java
@@ -2,18 +2,17 @@ package guru.springfamework.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-/**
- * Created by jt on 12/20/17.
- */
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig { // extends WebMvcConfigurationSupport {
 
     @Bean
     public Docket api(){
@@ -25,4 +24,13 @@ public class SwaggerConfig {
                 .pathMapping("/");
     }
 
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
